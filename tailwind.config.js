d223dfa3e228194a321ee198dc8b7ba3b8700140
Arfat <arfat6635@gmail.com>
2024-01-25 23:40:59 +0530

    Configuring tailwind & Adding template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,ts}'], //Help purge to remove unsed classes 
   theme: {
     extend: {},
   },
