1127757b036e23322e4f713e6bb6a42f946c9a3b
Arfat <arfat6635@gmail.com>
2024-01-27 19:29:02 +0530

    auth component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserModule,NavComponent],
+  imports: [RouterOutlet,UserModule,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
