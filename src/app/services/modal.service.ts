6756d486ff82f82fea6c6e065eff2a5ddcc541de
Arfat <arfat6635@gmail.com>
2024-02-04 19:47:29 +0530

    Fixed memory leak in auth component

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
 
+
+interface IModal{
+  id: string;
+  visible:boolean;
+}
+
+
 @Injectable({
   providedIn: 'root',
 })
+
 export class ModalService {
-  visible = false;
+  private modals:IModal[]=[];
+  private visible = false;
   constructor() {}
+
+  register(id:string){
+    this.modals.push(
+      {
+      id,
+      visible:false
+      }
+      )
+  }
+
+
+  unregister(id:string){
+      this.modals=this.modals.filter(
+        elements=>elements.id !== id 
+      )
+  }
+
+  isModalOpen(id:string){
+    return !!this.modals.find(element => element.id == id)?.visible //finds the id specified to modal
+  }
+
+  toggleModal(id:String){
+    let modal = this.modals.find(element => element.id==id)
+    if(modal){
+      modal.visible = !modal.visible
+    }
+  }
+
+  
+
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -7,7 +7,12 @@ import { ModalService } from '../../services/modal.service';
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
+  @Input() modalID ='';
   constructor(public modal:ModalService ){
-    console.log(modal.visible);
   }
-}
+
+  closeModal(){
+    this.modal.toggleModal(this.modalID)
+  }
+  
+} 
