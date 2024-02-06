c8d0dc52ff4759e82c48541a06445d630a98e8ea
Arfat <arfat6635@gmail.com>
2024-02-07 00:14:02 +0530

    optimizing modal

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrl: './tabs-container.component.css'
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+@ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
 
+ngAfterContentInit(): void {
+  
 }
+
+ }
